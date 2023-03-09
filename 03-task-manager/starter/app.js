61f4c321aef808bf790f7a2856cb28d4ea237352
diegocolonia <diegocolonia98@yahoo.com>
2023-03-08 20:50:34 -0500

    commit

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,8 +1,8 @@
-require('./db/connect');
+
 const express = require('express');
 const app = express();
 const tasks = require('./routes/tasks')
-
+const connectDB = require('./db/connect');
 
 //middleware
 app.use(express.json());
@@ -13,11 +13,17 @@ response.send('task manager app')
 });
 
 app.use('/api/v1/tasks',tasks);
-
-
 const port = 3000;
 
+const start = async () => {
+    try {
+        await connectDB();
+        app.listen(port, console.log(`server is listening to port ${port}...`));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
 
 
-
-app.listen(port,console.log(`server is listening to port ${port}...`))
--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -1,8 +1,23 @@
 const mongoose = require('mongoose');
 
-
 const connectingString = 'mongodb+srv://diego98343:3253633Aa@cluster0.ubwlczw.mongodb.net/taskManager?retryWrites=true&w=majority';
 
-mongoose.connect(connectingString)
-.then(()=>{ console.log('CONNECTED TO DATA BASE')})
-.catch((err)=>console.log(err));
+const connectDB = (url) => {
+    return mongoose.connect(connectingString,{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+ })
+}
+
+module.exports = connectDB;
+
+// mongoose.connect(connectingString,{
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//     useUnifiedTopology: true
+// })
+// .then(()=>{ console.log('CONNECTED TO DATA BASE')})
+// .catch((err)=>console.log(err));
