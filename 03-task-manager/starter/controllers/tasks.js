322192cca4f44cc68d7a779f249842433b3004f2
diegocolonia <diegocolonia98@yahoo.com>
2023-03-14 01:10:58 -0400

    commit

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -33,10 +33,6 @@ const getTask = async (req, res) => {
     }
 };
 
-const updateTask = (req, res) => {
-
-    res.send('update task');
-};
 
 const deleteTask = async (req, res) => {
     try {
@@ -46,14 +42,32 @@ const deleteTask = async (req, res) => {
         if (!task) {
             return res.status(404).json({ msg: `No task with id: ${taskID}` });
         } else {
-            return res.status(404).json({ msg: `${taskID} was deleted successfully` });
-            
+            return res.status(404).json({ msg: `${taskID} was deleted successfully` }); 
         }
         
-
     } catch (error) {
         res.status(500).json({ msg: error });
     }
+};
+
+
+const updateTask = async (req, res) => {
+    try {
+        const { id: taskID } = req.params;
+        
+        const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+            new:true,runValidators:true
+        });
+        if (!task) {
+            return res.status(404).json({msg: `No task with is ${taskID}`})
+        }
+        
+        res.status(200).json({ task });
+        
+    } catch(error) {
+          res.status(500).json({ msg: error });
+    }
+    
 };
 
 
