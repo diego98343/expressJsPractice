188ee214248f569878bb767a9c2e1d8367e16307
diegocolonia <diegocolonia98@yahoo.com>
2023-03-26 02:27:38 -0400

    commit

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -5,7 +5,12 @@ const { search } = require('../routes/products');
 const getAllProductsStatic = async (req, res) => {
     
 
-    const products = await Product.find({}).sort('name');
+    const products = await Product.find({price:{$gt: 100}})
+        .sort('name')
+        .select('name price')
+        .limit(10)
+        .skip(1)
+    
     res.status(200).json({ products, nbHits: products.length });
   
 }
@@ -13,7 +18,7 @@ const getAllProductsStatic = async (req, res) => {
 //creating a controller with custom querry 
 const getAllProducts = async (req, res) => {
 
-    const { featured,company,name,sort,fields } = req.query
+    const { featured,company,name,sort,fields, numericFilters } = req.query
     
     const queryObject = {}
     
@@ -44,6 +49,36 @@ const getAllProducts = async (req, res) => {
         const fieldList = fields.split(',').join(' ');
         result = result.select(fieldList);
     }
+
+    if (numericFilters) {
+
+        //we are converting numericFilter querry in mangos way to filter
+        const operatorMap = {
+            '>': '$gt',
+            '>=': '$gte',
+            '=': '$eq',
+            '<': '$lt',
+            '<=': '$lte',
+        }
+        const regEx = /\b(>|>=|=|<|<=)\b/g
+
+        let filters = numericFilters.replace(
+            regEx,
+            (match) => `-${operatorMap[match]}-`);
+        
+        filters = filters.split(',').forEach((item) => {
+            const [field, operator, value] = item.split('-');
+            if (options.includes(field)) {
+                queryObject[field] = { [operator]: Number(value) }
+            }
+        });
+    }
+
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
+
+    result = result.skip(skip).limit(limit);
 
     const products = await result
 
